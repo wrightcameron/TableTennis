61efdafc669f02a9d30b47555c05c961bdb86932
Cameron Wright <cameronwright@u.boisestate.edu>
2019-01-16 18:23:39 -0700

    Working on sockets, but changes on dev branch to stop changes to original functionality

--- TableTennis.go
+++ TableTennis.go
@@ -2,8 +2,9 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"net"
 	"os"
-	"strconv"
 	"strings"
 )
 
@@ -23,25 +24,57 @@ func main() {
 	}
 	//TODO Add error handling if the user does not add the right types.
 	serverHost := args[1]
-	portNum, err := strconv.Atoi(args[2])
-	if err != nil {
-		// handle error
-		fmt.Println(err)
-		os.Exit(-1)
-	}
+	portNum := args[2]
 
 	//Either launch the server or the client
 	if isServer {
-		server(serverHost, portNum)
+		server(portNum)
 	} else {
 		client(serverHost, portNum)
 	}
 }
 
-func server(serverHost string, port int) {
-
+func server(port string) {
+	port = ":" + port
+	tcpAddr, err := net.ResolveTCPAddr("tcp", port)
+	checkError(err)
+	listener, err := net.ListenTCP("tcp", tcpAddr)
+	checkError(err)
+	conn, err := listener.Accept()
+	if err != nil {
+		fmt.Println("Error")
+	}
+	fmt.Println("Connection established.")
+	for {
+		_, err = conn.Write([]byte("Ping"))
+		fmt.Println("wrote Ping")
+		//result, err := ioutil.ReadAll(conn)
+		//checkError(err)
+		//fmt.Println(string(result))
+	}
+	//conn.Close()
 }
 
-func client(serverHost string, port int) {
+func client(serverHost string, port string) {
 
+	serverHost = serverHost + ":" + port
+	tcpAddr, err := net.ResolveTCPAddr("tcp", serverHost)
+	checkError(err)
+	conn, err := net.DialTCP("tcp", nil, tcpAddr)
+	checkError(err)
+	for {
+		result, err := ioutil.ReadAll(conn)
+		checkError(err)
+		fmt.Println(string(result))
+		//_, err = conn.Write([]byte("Pong"))
+	}
+	//checkError(err)
+	//os.Exit(0)
+}
+
+func checkError(err error) {
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Fatal error: %s\n", err.Error())
+		os.Exit(1)
+	}
 }
